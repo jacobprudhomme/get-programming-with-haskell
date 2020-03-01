524dd789cfea30c19af6626dab5b09bc54b54ce7
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2020-02-29 19:12:10 -0800

    Unit 0 - Lesson 1

---
+++ unit0/lesson1/first_prog.hs
@@ -0,0 +1,37 @@
+#!/usr/bin/env stack
+-- stack exec ghci --resolver lts-14.20
+
+messyMain :: IO ()
+messyMain = do
+  print "Who is the email for?"
+  recipient <- getLine
+  print "What is the title?"
+  title <- getLine
+  print "Who is the author?"
+  author <- getLine
+  print(
+    "Dear " ++ recipient ++ ",\n" ++
+    "Thanks for buying " ++ title ++
+    "\nThanks,\n" ++
+    author
+    )
+
+toPart recipient = "Dear " ++ recipient ++ ",\n"
+
+bodyPart bookTitle = "Thanks for buying " ++ bookTitle ++ ".\n"
+
+fromPart author = "Thanks,\n" ++ author
+
+createEmail recipient bookTitle author =
+  toPart recipient ++
+  bodyPart bookTitle ++
+  fromPart author
+
+main = do
+  print "Who is the email for?"
+  recipient <- getLine
+  print "What is the title?"
+  title <- getLine
+  print "Who is the author?"
+  author <- getLine
+  print (createEmail recipient title author)
---
+++ unit0/lesson1/hello.hs
@@ -0,0 +1,8 @@
+#!/usr/bin/env stack
+-- stack exec ghci --resolver lts-14.20
+
+main = do
+  print "Hello, Jacob!"
+
+oldMain = do
+  print "Hello, world!"
