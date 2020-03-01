c0586a46a97e218362e833737c7b2332c9376b4b
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2020-02-29 19:52:28 -0800

    Unit 5 - Intro

---
+++ unit5/intro.hs
@@ -0,0 +1,11 @@
+#!/usr/bin/env stack
+-- stack exec ghci --resolver lts-14.20
+
+halve :: Int -> Double
+halve n = fromIntegral n / 2.0
+
+-- Would have to make a wrapper function like this very often
+-- to deal with all the different cases where Maybes can appear
+halveMaybe :: Maybe Int -> Maybe Double
+halveMaybe Nothing  = Nothing
+halveMaybe (Just n) = Just $ halve n
