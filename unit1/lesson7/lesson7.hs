96de5f609c1ddf79581fbc6f85d503234df74853
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2020-02-29 19:13:20 -0800

    Unit 1 - Lesson 7

---
+++ unit1/lesson7/lesson7.hs
@@ -0,0 +1,31 @@
+#!/usr/bin/env stack
+-- stack exec ghci --resolver lts-14.20
+
+myGCD a b =
+  if remainder == 0
+  then b
+  else myGCD b remainder
+  where remainder = a `mod` b
+
+sayAmount n =
+  case n of
+    1 -> "one"
+    2 -> "two"
+    _ -> "a bunch"
+
+sayAmount' 1 = "one"
+sayAmount' 2 = "two"
+sayAmount' _ = "a bunch"
+
+isEmpty [] = True
+isEmpty _  = False
+
+myHead (x:xs) = x
+myHead []     = error "Cannot take head of empty list"
+
+myTail (_:xs) = xs
+myTail []     = error "Cannot take tail of empty list"
+
+myGCD' a 0 = a
+myGCD' a b = myGCD b remainder
+  where remainder = a `mod` b
